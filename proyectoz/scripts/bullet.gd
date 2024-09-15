77d184c47e0e7a6e6c4c76c763d0673535de4143
LushoCallapa <luiscallapa2004@gmail.com>
2024-09-15 00:10:17 -0400

    Fixed

---
+++ proyectoz/scripts/bullet.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+@export var target: Vector2
+var speed = 40
+@export var direction: Vector2
+
+
+
+func _physics_process(delta):
+	print(direction)
+	linear_velocity = direction * speed
+	var distance = global_position.distance_to(target)
+	if distance < 1:
+		queue_free()
